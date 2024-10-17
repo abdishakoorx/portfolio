53cd533885cacbb379205d4992e198acda7d6c0b
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2024-10-18 00:14:52 +0300

    whites

--- app/_components/About.jsx
+++ app/_components/About.jsx
@@ -2,14 +2,44 @@ import React from 'react'
 
 function About() {
     return (
-        <div className='flex items-center justify-center pt-16'>
-            <div className='flex flex-col items-center justify-center max-w-3xl mx-auto space-y-8 text-center'>
-                <h1 className='text-4xl font-extrabold leading-tight text-gray-900 sm:text-5xl lg:text-6xl'>Hi! My name is 
-                <span className='ml-4 text-transparent bg-gradient-to-b from-purple-700 to-blue-400 bg-clip-text'>Abdishakoor Hassan</span>.</h1>
-                <h2 className='max-w-2xl text-xl text-gray-600'>I am a Software Developer specializing in full-stack web development with a passion for creating efficient, scalable, and user-friendly applications.</h2>
-            </div>
+        <div className="relative w-full overflow-hidden min-h-96">
+            {/* Grid background */}
+            <div
+                className="absolute inset-0"
+                style={{
+                    backgroundImage: `
+                  linear-gradient(to right, #DBDCDA 1px, transparent 1px),
+                  linear-gradient(to bottom, #DBDCDA 1px, transparent 1px)
+                `,
+                    backgroundSize: '64px 64px',
+                }}
+            />
+
+            {/* Radial gradient for blurring effect */}
+            <div
+                className="absolute inset-0"
+                style={{
+                    background: 'radial-gradient(circle, transparent 50%, #E7E7E7 50%)',
+                }}
+            />
+
+            {/* Content area */}
+            <main className="relative z-10 flex items-center justify-center h-full pt-16">
+                <div div className='flex items-center justify-center' >
+                    <div className='flex flex-col items-center justify-center max-w-3xl mx-auto space-y-8 text-center'>
+                        <h1 className='text-4xl font-extrabold leading-tight text-gray-950 sm:text-5xl lg:text-6xl'>Hi! My name is
+                            <span className='ml-4 text-transparent bg-gradient-to-b from-[#333333] to-[#dd1818] bg-clip-text'>
+                                Abdishakoor Hassan
+                            </span>.
+                        </h1>
+                        <h2 className='max-w-4xl text-xl tracking-wide text-gray-600'>I am a Software Developer specializing in full-stack web development with a passion for creating efficient, scalable, and user-friendly applications.</h2>
+                    </div>
+                </div>
+                
+            </main>
         </div>
     )
 }
 
 export default About
+
--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import About from "./_components/About";
 import Footer from "./_components/Footer";
 import Header from "./_components/Header";
 import Projects from "./_components/Projects";
+import TechTools from "./_components/TechTools";
 
 export default function Home() {
   return (
     <div className="px-10">
       <Header/>
       <About/>
+      <TechTools/>
       <Projects/>
       <Footer/>
     </div>
