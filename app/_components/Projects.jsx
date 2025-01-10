f50020139606c9c98cb9dc125893b12d1480c0f2
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-01-10 15:40:13 +0300

    refactor: update project images to webp format and enhance project card links

--- app/_components/Projects.jsx
+++ app/_components/Projects.jsx
@@ -1,13 +1,14 @@
 'use client'
 
-import React, { useEffect } from 'react'
-import { motion, useAnimation, useInView } from 'framer-motion'
+import React from 'react'
+import { motion } from 'framer-motion'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { ExternalLink } from "lucide-react"
 import Image from 'next/image'
 import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import Link from 'next/link'
 
 const projects = [
   {
@@ -16,15 +17,15 @@ const projects = [
     technologies: ["NextJs", "Hygraph CMS", "TailwindCSS", "GraphQL", "Kinde"],
     githubUrl: "https://github.com/abdishakoorx/doctor_appoinment",
     liveUrl: "https://doctorwhere.vercel.app/",
-    imageUrl: "/doctorwhere.png"
+    imageUrl: "/doctorwhere.webp"
   },
   {
     title: "AI Travel Planner",
     description: "A web app with AI that generates smart recommendations for trips.",
-    technologies: ["React", "NodeJs", "Gemini AI", "Vite", "Tailwind CSS"],
+    technologies: ["Vite", "JavaScript", "Gemini AI", "Tailwind CSS"],
     githubUrl: "https://github.com/abdishakoorx/trip_planner",
     liveUrl: "https://greatertrips.vercel.app/",
-    imageUrl: "/greatertrips.png"
+    imageUrl: "/greatertrips.webp"
   },
   {
     title: "Property Management System",
@@ -32,53 +33,25 @@ const projects = [
     technologies: ["React", "Node.js", "NextJs", "Supabase", "Clerk"],
     githubUrl: "https://github.com/abdishakoorx/primestate",
     liveUrl: "https://primestate-eta.vercel.app/",
-    imageUrl: "/primestate.png"
+    imageUrl: "/primestate.webp"
   },
-  // {
-  //   title: "Food Scoring System",
-  //   description: "Choose your food, submit them, and receive a score with feedback.",
-  //   technologies: ["React", "NodeJs", "Python", "TailwindCSS", "GraphQL"],
-  //   githubUrl: "https://github.com/abdishakoorx/whiteplates",
-  //   liveUrl: "https://whiteplates.vercel.app/",
-  //   imageUrl: "/whiteplates.png"
-  // },
+  {
+    title: "Video Creator",
+    description: "AI-powered platform for creating professional videos with ease.",
+    technologies: ["NextJs", "TypeScript", "NeonPg", "TailwindCSS", "Clerk"],
+    githubUrl: "https://github.com/abdishakoorx/meeedia",
+    liveUrl: "https://meeeedia.vercel.app/",
+    imageUrl: "/meeedia.webp"
+  },
 ]
 
 const ProjectCard = ({ project, index }) => {
-  const ref = React.useRef(null)
-  const isInView = useInView(ref, { once: false, margin: "-100px" })
-  const controls = useAnimation()
-
-  useEffect(() => {
-    if (isInView) {
-      controls.start("visible")
-    } else {
-      controls.start("hidden")
-    }
-  }, [isInView, controls])
-
-  const cardVariants = {
-    hidden: {
-      opacity: 0,
-      y: 50,
-    },
-    visible: {
-      opacity: 1,
-      y: 0,
-      transition: {
-        duration: 0.5,
-        ease: "easeOut",
-        delay: index * 0.1
-      }
-    }
-  }
-
   return (
     <motion.div
-      ref={ref}
-      variants={cardVariants}
-      initial="hidden"
-      animate={controls}
+      initial={{ opacity: 0, y: 20 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5, delay: index * 0.1 }}
+      viewport={{ once: true, margin: "-50px" }}
     >
       <Card className="flex flex-col h-full bg-transparent">
         <CardHeader>
@@ -86,16 +59,18 @@ const ProjectCard = ({ project, index }) => {
           <CardDescription className='text-gray-700'>{project.description}</CardDescription>
         </CardHeader>
         <CardContent className="flex-grow">
-          <Image
-            height={800}
-            width={800}
-            src={project.imageUrl}
-            alt={`Screenshot of ${project.title}`}
-            className="mb-4 rounded-md"
-          />
+          <Link href={project.liveUrl} target="_blank" rel="noopener noreferrer">
+            <Image
+              height={800}
+              width={800}
+              src={project.imageUrl}
+              alt={`Screenshot of ${project.title}`}
+              className="mb-4 rounded-md"
+            />
+          </Link>
           <div className="flex flex-wrap gap-2 mb-4">
             {project.technologies.map((tech, techIndex) => (
-              <Badge key={techIndex} className="text-gray-900 bg-teal-600 hover:bg-teal-500">
+              <Badge key={techIndex} className="text-[#2a2d6f] bg-teal-300 hover:bg-teal-500">
                 {tech}
               </Badge>
             ))}
@@ -103,16 +78,16 @@ const ProjectCard = ({ project, index }) => {
         </CardContent>
         <CardFooter className="flex justify-between">
           <Button size="sm" asChild className='text-gray-800 bg-transparent border-2 border-gray-700 hover:bg-accent'>
-            <a href={project.githubUrl} target="_blank" rel="noopener noreferrer">
+            <Link href={project.githubUrl} target="_blank" rel="noopener noreferrer">
               <GitHubLogoIcon className="w-4 h-4 mr-2" />
               GitHub
-            </a>
+            </Link>
           </Button>
           <Button size="sm" asChild className='bg-gray-900 hover:bg-gray-700'>
-            <a href={project.liveUrl} target="_blank" rel="noopener noreferrer">
+            <Link href={project.liveUrl} target="_blank" rel="noopener noreferrer">
               <ExternalLink className="w-4 h-4 mr-2" />
               Live Demo
-            </a>
+            </Link>
           </Button>
         </CardFooter>
       </Card>
@@ -126,9 +101,10 @@ const Projects = () => {
       <div className="mx-auto max-w-7xl">
         <motion.h2
           className="mb-8 text-4xl font-extrabold text-center text-gray-900"
-          initial={{ opacity: 0, y: -20 }}
-          animate={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 10 }}
+          whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
+          viewport={{ once: true }}
         >
           My Projects
         </motion.h2>
