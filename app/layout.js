52dd7b8c0ba688e760d523c0cba69eb81269de77
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-04-21 23:34:15 +0300

    fix: update site title to Abdishakoor Hassan

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio",
+  title: "Abdishakoor Hassan",
   description: "Let's work together",
 };
 
