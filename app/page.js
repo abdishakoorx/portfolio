9b4775f1c2a5f782d95c27135c3958db1c230ad3
Hassan Abdishakoor <hassanabdishakoor@gmail.com>
2025-08-07 00:05:01 +0300

    feat: add new sections to replace the old ones with the new design using dark mode

--- app/page.js
+++ app/page.js
@@ -1,10 +1,22 @@
-import Dashboard from "./Dashboard";
+import Navigation from "@/components/sections/Navigation";
+import HeroSection from "@/components/sections/HeroSection";
+import AboutSection from "@/components/sections/AboutSection";
+import ExperienceSection from "@/components/sections/ExperienceSection";
+import ProjectsSection from "@/components/sections/ProjectSection";
+import ContactSection from "@/components/sections/ContactSection";
+import Footer from "@/components/sections/Footer";
 
 
 export default function Home() {
   return (
     <main>
-      <Dashboard/>
+      <Navigation />
+      <HeroSection />
+      <AboutSection />
+      <ExperienceSection />
+      <ProjectsSection />
+      <ContactSection />
+      <Footer />
     </main>
   );
 }
